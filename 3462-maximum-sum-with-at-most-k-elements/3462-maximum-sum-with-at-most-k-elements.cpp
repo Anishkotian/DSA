03356da6e841f14486531842eeb85dfaf6212fe8
Anish Kotian <anishkotyan63@gmail.com>
2026-08-29 11:01:00 +0530

    Time: 208 ms (20.61%), Space: 109.9 MB (52.12%) - LeetHub

---
+++ 3462-maximum-sum-with-at-most-k-elements/3462-maximum-sum-with-at-most-k-elements.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long maxSum(vector<vector<int>>& grid, vector<int>& limits, int k) {
+        vector<int>v;
+        for(int i=0;i<grid.size();i++){
+            sort(grid[i].rbegin(),grid[i].rend());
+            for(int j=0;j<limits[i];j++){
+                v.push_back(grid[i][j]);
+            }
+        }
+        sort(v.rbegin(),v.rend());
+        long long ans=0;
+        for(int i=0;i<k;i++){
+            ans+=v[i];
+        }
+        return ans;
+    }
+};
