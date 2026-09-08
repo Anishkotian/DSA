b8e105434158fba76da6f5157a63184450bb7c6b
Anish Kotian <anishkotyan63@gmail.com>
2026-09-08 22:24:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long ans(vector<int>& nums,int target){
+        int left=0;
+        int right=nums.size()-1;
+        long long count=0;
+        while(left<right){
+            if(nums[left]+nums[right]<=target){
+                count+=right-left;
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return count;
+    }
+    long long countFairPairs(vector<int>& nums,int lower,int upper) {
+        sort(nums.begin(),nums.end());
+        long long res1=ans(nums,upper);
+        long long res2=ans(nums,lower-1);
+        return res1-res2+1;
+    }
+};
