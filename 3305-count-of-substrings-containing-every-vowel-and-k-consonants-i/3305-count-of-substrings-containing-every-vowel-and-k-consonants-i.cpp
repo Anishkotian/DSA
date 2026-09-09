50b62a7725907694a3f0d67053d9eaf1f1858144
Anish Kotian <anishkotyan63@gmail.com>
2026-09-09 21:15:24 +0530

    Time: 3 ms (80.07%), Space: 10.5 MB (52.23%) - LeetHub

--- 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
+++ 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-    bool find(char c){
+    bool vowel(char c){
         return c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
     }
 
-    long long countOfSubstrings(string word, int k) {
-        long long ans=0;
-        for(int i=0;i<word.size();i++){
-            set<char>s;
-            int cons=0;
-            for(int j=i;j<word.size();j++){
-                if(find(word[j])){
-                    s.insert(word[j]);
+    long long fun(string s,int k){
+        unordered_map<char,int> m;
+        int l=0,c=0;
+        long long ans=0;              
+        for(int r=0;r<s.size();r++){
+            if(vowel(s[r]))
+                m[s[r]]++;
+            else
+                c++;
+            while(c>=k&&m.size()==5){
+                ans+=s.size()-r;
+                if(vowel(s[l])){
+                    m[s[l]]--;
+                    if(m[s[l]]==0)
+                        m.erase(s[l]);
                 }
-                else{
-                    cons++;
-                }
-                if(cons>k){
-                    break;
-                }
-                if(s.size()==5&&cons==k){
-                    ans++;
-                }
+                else
+                    c--;
+                l++;
             }
         }
         return ans;
+    }
+
+    long long countOfSubstrings(string word,int k) {
+        return fun(word,k)-fun(word,k+1);
     }
 };
